70d128faec2241e9dc7f7198bed394a3f955ef66
Silas L. Pascoal <slpascoal01@gmail.com>
2024-09-17 17:32:17 -0300

    Configuração do CORS
    CORS configurado com acesso liberado, por fins de desenvolvimento

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const rotaLivro = require("./routes/route")
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
+app.use(cors({origin: "*"}))
 
 app.use('/livros', rotaLivro)
 
